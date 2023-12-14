c8bb466587935490fd0d9e50b7709cbcce93d6aa
laurenciopaez <laurencio314gmail.com>
2023-12-13 23:00:43 -0300

    Prev and next buttons -> style

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,17 @@
 //this comp was a class component, now its a functional component.
 
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import CardCharacter from './Card';
-import { connect } from 'react-redux';
+import CardCharacter from "./Card";
+import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
 
-const Cards = ({characters}) => {
-
+const Cards = ({ characters }) => {
   const itemsPerPage = 20;
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-  }, [characters]);
-
+  useEffect(() => {}, [characters]);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
@@ -24,35 +22,41 @@ const Cards = ({characters}) => {
   const charactersToShow = characters.slice(startIndex, endIndex);
 
   return (
-    <Row xs={1} md={4} className='g-4'>
-      {charactersToShow.map(({ id, name, species, gender, image, origin, status }) => (
-        <Col key={id}>
-          <CardCharacter
-            id={id}
-            name={name}
-            species={species}
-            gender={gender}
-            image={image}
-            origin={origin.name}
-            status={status}
-            // onClose={() => handleClick(id)}
-          />
-        </Col>
-      ))}
-      <div className='pagination'>
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Anterior
-        </button>
-        <span>{currentPage}</span>
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={endIndex >= characters.length}
-        >
-          Siguiente
-        </button>
+    <Row xs={1} md={4} className="g-4">
+      {charactersToShow.map(
+        ({ id, name, species, gender, image, origin, status }) => (
+          <Col key={id}>
+            <CardCharacter
+              id={id}
+              name={name}
+              species={species}
+              gender={gender}
+              image={image}
+              origin={origin.name}
+              status={status}
+              // onClose={() => handleClick(id)}
+            />
+          </Col>
+        )
+      )}
+      <div className="w-full justify-center flex">
+        <div className="pr-2 pl-2 bg-slate-50 justify-center flex rounded-md items-center">
+          <Button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="bg-blue-500 mb-2 mt-2 mr-2 w-24"
+          >
+            Previous
+          </Button>
+          <span className="text-black text-2xl font-semibold">{currentPage}</span>
+          <Button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={endIndex >= characters.length}
+            className="bg-blue-500 mb-2 mt-2 ml-2 w-24"
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </Row>
   );
@@ -60,7 +64,6 @@ const Cards = ({characters}) => {
 
 const mapStateToProps = (state) => ({
   characters: state.characters,
-})
-
+});
 
 export default connect(mapStateToProps, {})(Cards);
